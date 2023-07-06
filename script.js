bb1d3e805f1b14c30bd2c6dec80875b672ddc237
Smaddermand <50303469+Smaddermand@users.noreply.github.com>
2023-07-06 11:43:37 +0200

    Create button and drawing function

--- script.js
+++ script.js
@@ -1,26 +1,45 @@
 function createGrid(n){
+    let cellSize = 800/n;
 for(let i = 0; i < n; i++){
         for(let j = 0; j < n; j++){
         const newDiv = document.createElement("div"); //create div
         newDiv.classList.add("grid"); //add grid class
         container.appendChild(newDiv);
-
-    //  document.getElementsByClassName("grid").style.color = "blue"
     }
-    
-    
-    //const currentDiv = document.getElementById("div");
-    //document.body.insertBefore(newDiv, currentDiv);
+
 }
     let elements = document.getElementsByClassName("grid");
     for (let i = 0; i < elements.length; i++) {
-        elements[i].style.width = 800/n + "px"; // change the width here
-        elements[i].style.height = 800/n + "px"; // change the height here
+        elements[i].style.width = cellSize + "px"; // change the width here
+        elements[i].style.height = cellSize + "px"; // change the height here
     }
+    addMouseOverEffect();
+};
 
+function addMouseOverEffect() {
+    let elements = document.getElementsByClassName("grid");
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].addEventListener("mouseover", function() {
+            this.style.backgroundColor = "blue"; // change color as needed
+        });
+    }
 }
 
 
-createGrid(32);
+function draw(){
+     gridSize = prompt("please select size of grid");
+    if(gridSize >= 100){
+        alert("It's to big mister");
+        draw();
+    } else {
+    createGrid(gridSize);
+    }
+}
+
+//createGrid(16);
+const startDraw = document.getElementById("startButton");
+startDraw.addEventListener("click", draw);
+
+
 
 
